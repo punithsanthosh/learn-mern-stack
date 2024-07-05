ea489c634a126ecc9ed6073636a738bb993a3e6b
punithsanthosh <punithsanthosh10@gmail.com>
2024-07-05 22:28:01 +0530

    updating the backend

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,12 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import Header from "../src/components/header";
-import Card from "../src/components/card";
 import Task from "./components/task";
-import Input from "./components/input";
 import About from "./components/About";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
@@ -14,18 +12,17 @@ function App() {
       <main>
         <ul>
           <li>
-            <Link to={"/"}>Task</Link>
+            <Link to="/">Task</Link>
           </li>
           <li>
-            <Link to={"/about"}>About</Link>
+            <Link to="/about">About</Link>
           </li>
         </ul>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Task />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<Task />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </main>
     </div>
   );
--- my-react-app/src/components/input.js
+++ my-react-app/src/components/input.js
@@ -1,37 +1,33 @@
 import { useEffect, useState } from "react";
 
 const Input = (props) => {
-
-    const [inputText,setInputText] = useState();
-    const handleInput = (e) => {
-            setInputText(e.target.value);
-            console.log(e.target.value);
-        }    
-const handleSubmit = (e) => {
-    const isValid=true;
-    if(!isValid)
-        {
-            alert("invalid text")
-        }
-        else
-        {
-            props.addTaskHandler(inputText);
-            setInputText("")
-        }
-        e.preventDefault();
-        props.addTaskHandler(inputText);
+  const [inputText, setInputText] = useState();
+  const handleInput = (e) => {
+    setInputText(e.target.value);
+    console.log(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    const isValid = true;
+    if (!isValid) {
+      alert("invalid text");
+    } else {
+      props.addTaskHandler(inputText);
+      setInputText("");
     }
+    e.preventDefault();
+    props.addTaskHandler(inputText);
+  };
 
-    return (
-            <form onSubmit={handleSubmit}>
-                <input
-                 type="text" 
-                 placeholder="enter your task here"
-                value={inputText}
-                onChange={handleInput}
-            />
-                <button type="submit">Add Task</button>
-            </form>
-        )
-    }
-    export default Input;
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="enter your task here"
+        value={inputText}
+        onChange={handleInput}
+      />
+      <button type="submit">Add Task</button>
+    </form>
+  );
+};
+export default Input;
